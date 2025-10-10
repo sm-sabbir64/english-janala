62b53fd5288ae56aed35a57b9c00740745af74db
Sabbir Hossain <sm.sabbir9671@gmail.com>
2025-10-11 00:49:50 +0600

    added lessons with API

---
+++ script/index.js
@@ -0,0 +1,28 @@
+const loadLessons = () => {
+  fetch("https://openapi.programming-hero.com/api/levels/all") //promise of response
+    .then((res) => res.json()) //promise of the json data
+    .then((json) => displayLesson(json.data));
+};
+const displayLesson = (lessons) => {
+  console.log(lessons);
+  //1. Get the container & empty
+  const levelContainer = document.getElementById("level-container");
+  levelContainer.innerHTML="";
+
+  //2. get into every lessons
+  for (let lesson of lessons) {
+    //      3. create element
+    console.log(lesson);
+    const btnDiv = document.createElement("div");
+    btnDiv.innerHTML = `
+        <button class="btn btn-outline btn-primary">
+        <i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}
+        </button>
+        `;
+
+    //      4. append into container
+
+    levelContainer.append(btnDiv);
+  }
+};
+loadLessons();
